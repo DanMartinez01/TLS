2ec4768ab8c02e25ad6fc2649d85e4b4782a849a
Daniela <martinezdl_@hotmail.com>
2024-03-28 14:00:51 -0300

    add btn to translate texts

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
-import Link from "next/link";
+import Image from "next/image";
 // Import react scroll
 import { Link as LinkScroll } from "react-scroll";
-import ButtonOutline from "../misc/ButtonOutline.";
-import LogoVPN from "../../public/assets/Logo.svg";
+import useCustomTranslation from "../../utils/useCustomTranslation";
+import LanguageSelector from "../LanguageSelector";
 
 const Header = () => {
   const [activeLink, setActiveLink] = useState(null);
@@ -13,6 +13,9 @@ const Header = () => {
       setScrollActive(window.scrollY > 20);
     });
   }, []);
+
+  const { t } = useCustomTranslation();
+
   return (
     <>
       <header
@@ -23,12 +26,15 @@ const Header = () => {
       >
         <nav className="max-w-screen-xl px-6 sm:px-8 lg:px-16 mx-auto grid grid-flow-col py-3 sm:py-4">
           <div className="col-start-1 col-end-2 flex items-center">
-            <LogoVPN className="h-8 w-auto" />
+            {/* <Image src="/assets/logo.png" width={180} height={100} /> */}
+            <h1 className="text-green font-bold text-xl">
+              TransLingual Solutions
+            </h1>
           </div>
           <ul className="hidden lg:flex col-start-4 col-end-8 text-black-500  items-center">
             <LinkScroll
               activeClass="active"
-              to="about"
+              to="/"
               spy={true}
               smooth={true}
               duration={1000}
@@ -44,6 +50,8 @@ const Header = () => {
             >
               Home
             </LinkScroll>
+            {/* <h1>{t("welcome")}</h1> */}
+
             <LinkScroll
               activeClass="active"
               to="feature"
@@ -56,7 +64,7 @@ const Header = () => {
               className={
                 "px-4 py-2 mx-2 cursor-pointer animation-hover inline-block relative" +
                 (activeLink === "feature"
-                  ? " text-orange-500 animation-active "
+                  ? " text-green animation-active "
                   : " text-black-500 hover:text-green ")
               }
             >
@@ -64,7 +72,7 @@ const Header = () => {
             </LinkScroll>
             <LinkScroll
               activeClass="active"
-              to="pricing"
+              to=""
               spy={true}
               smooth={true}
               duration={1000}
@@ -75,10 +83,10 @@ const Header = () => {
                 "px-4 py-2 mx-2 cursor-pointer animation-hover inline-block relative" +
                 (activeLink === "pricing"
                   ? " text-orange-500 animation-active "
-                  : " text-black-500 hover:text-orange-500 ")
+                  : " text-black-500 hover:text-green ")
               }
             >
-              Servicios
+              {t("services")}
             </LinkScroll>
             <LinkScroll
               activeClass="active"
@@ -92,12 +100,13 @@ const Header = () => {
               className={
                 "px-4 py-2 mx-2 cursor-pointer animation-hover inline-block relative" +
                 (activeLink === "testimoni"
-                  ? " text-orange-500 animation-active "
-                  : " text-black-500 hover:text-orange-500 ")
+                  ? " text-green animation-active "
+                  : " text-black-500 hover:text-green")
               }
             >
-              Testimonios
+              {t("testimonies")}
             </LinkScroll>
+            <LanguageSelector />
           </ul>
           <div className="col-start-10 col-end-12 font-medium flex justify-end items-center">
             {/* <Link href="/">
@@ -108,19 +117,16 @@ const Header = () => {
             {/* <ButtonOutline>
               <a to="Form"> Solicitar Traducción</a>
             </ButtonOutline> */}
-            <Link
-              href={"/email-template"}
-              className="font-medium tracking-wide py-2 px-5 sm:px-8 border border-green text-green bg-white-500 outline-none rounded-l-full rounded-r-full capitalize hover:bg-green hover:text-white-500 transition-all hover:shadow-greeb"
-            >
+            {/* <Link className="font-medium tracking-wide py-2 px-5 sm:px-8 border border-green text-green bg-white-500 outline-none rounded-l-full rounded-r-full capitalize hover:bg-green hover:text-white-500 transition-all hover:shadow-greeb">
               Solicitar Traducción
-            </Link>
+            </Link> */}
           </div>
         </nav>
       </header>
       {/* Mobile Navigation */}
 
-      <nav className="fixed lg:hidden bottom-0 left-0 right-0 z-20 px-4 sm:px-8 shadow-t ">
-        <div className="bg-white-500 sm:px-3">
+      <nav className="w-full fixed lg:hidden bottom-0 left-0 right-0 z-20  shadow-t ">
+        <div className="bg-white-500 px-4 sm:px-3">
           <ul className="flex w-full justify-between items-center text-black-500">
             <LinkScroll
               activeClass="active"
@@ -134,14 +140,14 @@ const Header = () => {
               className={
                 "mx-1 sm:mx-2 px-3 sm:px-4 py-2 flex flex-col items-center text-xs border-t-2 transition-all " +
                 (activeLink === "about"
-                  ? "  border-green text-orange-500"
+                  ? "  border-green text-green"
                   : " border-transparent")
               }
             >
               <svg
                 className="w-6 h-6"
                 fill="none"
-                stroke="currentColor"
+                stroke="green"
                 viewBox="0 0 24 24"
                 xmlns="http://www.w3.org/2000/svg"
               >
@@ -152,9 +158,9 @@ const Header = () => {
                   d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                 />
               </svg>
-              About
+              Home
             </LinkScroll>
-            <LinkScroll
+            {/* <LinkScroll
               activeClass="active"
               to="feature"
               spy={true}
@@ -173,7 +179,7 @@ const Header = () => {
               <svg
                 className="w-6 h-6"
                 fill="none"
-                stroke="currentColor"
+                stroke="green"
                 viewBox="0 0 24 24"
                 xmlns="http://www.w3.org/2000/svg"
               >
@@ -185,7 +191,7 @@ const Header = () => {
                 />
               </svg>
               Feature
-            </LinkScroll>
+            </LinkScroll> */}
             <LinkScroll
               activeClass="active"
               to="pricing"
@@ -198,14 +204,14 @@ const Header = () => {
               className={
                 "mx-1 sm:mx-2 px-3 sm:px-4 py-2 flex flex-col items-center text-xs border-t-2 transition-all " +
                 (activeLink === "pricing"
-                  ? "  border-orange-500 text-orange-500"
+                  ? "  border-green text-green"
                   : " border-transparent ")
               }
             >
               <svg
                 className="w-6 h-6"
                 fill="none"
-                stroke="currentColor"
+                stroke="green"
                 viewBox="0 0 24 24"
                 xmlns="http://www.w3.org/2000/svg"
               >
@@ -216,7 +222,7 @@ const Header = () => {
                   d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                 />
               </svg>
-              Pricing
+              Como funciona
             </LinkScroll>
             <LinkScroll
               activeClass="active"
@@ -230,14 +236,14 @@ const Header = () => {
               className={
                 "mx-1 sm:mx-2 px-3 sm:px-4 py-2 flex flex-col items-center text-xs border-t-2 transition-all " +
                 (activeLink === "testimoni"
-                  ? "  border-orange-500 text-orange-500"
+                  ? "  border-green text-green"
                   : " border-transparent ")
               }
             >
               <svg
                 className="w-6 h-6"
                 fill="none"
-                stroke="currentColor"
+                stroke="green"
                 viewBox="0 0 24 24"
                 xmlns="http://www.w3.org/2000/svg"
               >
@@ -248,8 +254,9 @@ const Header = () => {
                   d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
                 />
               </svg>
-              Testimonial
+              {t("testimonies")}
             </LinkScroll>
+            <LanguageSelector />
           </ul>
         </div>
       </nav>
