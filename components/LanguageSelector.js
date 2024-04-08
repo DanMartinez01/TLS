5a8b35d8f9beb1681f94508c9709a51f66ae5804
Daniela <martinezdl_@hotmail.com>
2024-04-08 20:10:48 -0300

    change bg color

--- components/LanguageSelector.js
+++ components/LanguageSelector.js
@@ -23,7 +23,7 @@ function LanguageSelector() {
           <button
             type="button"
             onClick={toggleDropdown}
-            className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            className="inline-flex justify-center w-full rounded-md border border-gray-100 shadow-sm px-4 py-2 bg-white-500 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             id="options-menu"
             aria-haspopup="true"
             aria-expanded="true"
@@ -49,7 +49,7 @@ function LanguageSelector() {
         {/* Dropdown menu */}
         {isOpen && (
           <div
-            className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 z-10"
+            className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white-500 ring-1 ring-black ring-opacity-5 z-10"
             role="menu"
             aria-orientation="vertical"
             aria-labelledby="options-menu"
