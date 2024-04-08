ca7403328f557721b94182cbf96d098255609cc3
Daniela <martinezdl_@hotmail.com>
2024-04-08 20:12:01 -0300

    add new styles

--- components/Feature.js
+++ components/Feature.js
@@ -18,22 +18,23 @@ const Feature = () => {
 
   return (
     <div
-      // className="max-w-screen-xl mt-8 mb-6 sm:mt-14 sm:mb-14 px-6 sm:px-8 lg:px-16 mx-auto bg-green"
-      className="w-full h-2/4 mb-6  sm:mb-14 py-6 px-6 sm:px-8 lg:px-16  bg-green py-20"
+      className="w-full h-2/4   sm:mb-14 py-16 px-6 sm:px-8 lg:px-16  bg-green"
       id="feature"
     >
-      <div className="grid grid-flow-row sm:grid-flow-col grid-cols-1 sm:grid-cols-2 gap-8 items-center lg:relative">
+      <div className="flex flex-col xl:flex-row justify-center items-center">
         <ScrollAnimationWrapper>
           <motion.div
-            className="flex flex-col items-end justify-center sm:mx-auto ml-auto w-full lg:w-9/12 py-2"
+            className="flex flex-col w-full xl:w-3/4"
             variants={scrollAnimation}
           >
-            <h3 className="text-3xl lg:text-5xl font-medium leading-relaxed text-white-500">
-              We Provide Many Features You Can Use
+            <h3 className="text-3xl lg:text-5xl font-medium leading-relaxed text-white-500 lg:mb-4">
+              Translingual Solutions
             </h3>
-            <p className="my-4 text-white-500">{t("aboutUsText")}</p>
+            <p className="my-8 text-white-500 lg:leading-loose">
+              {t("aboutUsText")}
+            </p>
 
-            <ul className="text-white-500 self-start list-inside ml-8">
+            {/* <ul className="text-white-500 self-start list-inside ml-8">
               {features.map((feature, index) => (
                 <motion.li
                   className="relative circle-check custom-list"
@@ -50,14 +51,11 @@ const Feature = () => {
                   {feature}
                 </motion.li>
               ))}
-            </ul>
+            </ul> */}
           </motion.div>
         </ScrollAnimationWrapper>
-        <ScrollAnimationWrapper className="flex w-full">
-          <motion.div
-            className="h-2/6 w-2/6 sm:hidden lg:absolute  lg:-right-20 lg:top-2"
-            variants={scrollAnimation}
-          >
+        <ScrollAnimationWrapper className="hidden xl:flex xl:w-full">
+          <motion.div className="h-96 w-96" variants={scrollAnimation}>
             <Image
               src="/assets/logo-2.png"
               alt="Translingual Solutions"
