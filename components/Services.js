50aa942d177c9fbcef992c22422498d6f55927d1
Daniela <martinezdl_@hotmail.com>
2024-04-13 16:05:06 -0300

    change in styles

--- components/Services.js
+++ components/Services.js
@@ -1,198 +1,65 @@
-import React, { useMemo } from "react";
-import getScrollAnimation from "../utils/getScrollAnimation";
-import ScrollAnimationWrapper from "./Layout/ScrollAnimationWrapper";
+import React from "react";
+import { useTranslation } from "react-i18next";
 import { motion } from "framer-motion";
 import { FaCheckCircle } from "react-icons/fa";
+
 const Services = () => {
-  const scrollAnimation = useMemo(() => getScrollAnimation(), []);
+  const { t } = useTranslation();
+
   return (
     <div
-      className="w-full mx-auto justify-center  flex-row  pb-10 lg:px-8 lg:max-w-7xl "
+      className="w-full mx-auto justify-center flex-row pb-10 lg:px-8 lg:max-w-7xl"
       id="services"
     >
-      <ScrollAnimationWrapper>
-        <motion.h3
-          variants={scrollAnimation}
-          className="text-center mb-16 text-2xl sm:text-3xl lg:text-4xl font-medium text-black-600 leading-relaxed"
-        >
-          Services & Industries
-        </motion.h3>
-      </ScrollAnimationWrapper>
+      <motion.h3 className="text-center mb-16 text-2xl sm:text-3xl lg:text-4xl font-medium text-black-600 leading-relaxed">
+        {t("servtitle")}
+      </motion.h3>
 
-      <div className="flex flex-col justify-center md:flex-row lg:flex-row xl:flex-row pb-8">
-        <ScrollAnimationWrapper>
-          {/* <div className="flex flex-col w-2/5 items-center"> */}
-          <motion.div
-            variants={scrollAnimation}
-            className="flex flex-col w-3/4 items-center  mx-auto mt-4 "
-            whileHover={{
-              scale: 1.1,
-              transition: {
-                duration: 0.1,
-              },
-            }}
-          >
-            <div>
-              <h2 className="text-2xl  text-center font-bold-700 md:text-start lg:text-start xl:text-start sm:text-3xl mb-8">
-                Services:
-              </h2>
-              <div className="my-4 space-y-12 border rounded-lg  border-gray-500 border-2  py-6">
-                <div className="flex items-center">
-                  <div className="flex-shrink-0">
-                    <div className="flex items-center justify-center w-12 h-12 rounded-md">
-                      <FaCheckCircle size={20} color="green" />
-                    </div>
-                  </div>
-                  <div className="ml-4">
-                    <h4 className="text-md font-medium leadi ">
-                      Certified and Sworn Translations, legally validated at
-                      Sworn Translators’ Association of the City of Buenos
-                      Aires*
-                    </h4>
+      <div className="w-full px-2 flex flex-col justify-between md:flex-row lg:flex-row xl:flex-row pb-8">
+        {/* Services Card */}
+        <motion.div className="flex flex-col items-center mt-4 w-2/4">
+          <h2 className="text-2xl text-center font-bold-700 md:text-start lg:text-start xl:text-start sm:text-3xl mb-8">
+            {t("servsubtitle")}
+          </h2>
+          <div className="my-4 space-y-10 border rounded-lg border-gray-500 border-2 py-6 w-3/4 h-full">
+            {t("servicesList", { returnObjects: true }).map((item, index) => (
+              <div key={index} className="flex items-center">
+                <div className="flex-shrink-0">
+                  <div className="flex items-center justify-center w-12 h-12 rounded-md">
+                    <FaCheckCircle size={20} color="#486f60" />
                   </div>
                 </div>
-                <div className="flex items-center">
-                  <div className="flex-shrink-0">
-                    <div className="flex items-center justify-center w-12 h-12 rounded-md">
-                      <FaCheckCircle size={20} color="green" />
-                    </div>
-                  </div>
-                  <div className="ml-4">
-                    <h4 className="text-md font-medium leadi ">
-                      Scientific and Technical Translations
-                    </h4>
-                  </div>
-                </div>
-                <div className="flex items-center">
-                  <div className="flex-shrink-0">
-                    <div className="flex items-center justify-center w-12 h-12 rounded-md">
-                      <FaCheckCircle size={20} color="green" />
-                    </div>
-                  </div>
-                  <div className="ml-4">
-                    <h4 className="text-lg font-medium leadi ">Localization</h4>
-                  </div>
-                </div>
-                <div className="flex items-center">
-                  <div className="flex-shrink-0">
-                    <div className="flex items-center justify-center w-12 h-12 rounded-md">
-                      <FaCheckCircle size={20} color="green" />
-                    </div>
-                  </div>
-                  <div className="ml-4">
-                    <h4 className="text-md font-medium leadi ">
-                      Transcreation
-                    </h4>
-                  </div>
-                </div>
-                <div className="flex items-center">
-                  <div className="flex-shrink-0">
-                    <div className="flex items-center justify-center w-12 h-12 rounded-md">
-                      <FaCheckCircle size={20} color="green" />
-                    </div>
-                  </div>
-                  <div className="ml-4">
-                    <h4 className="text-md font-medium leadi ">
-                      Editing and Proofreading{" "}
-                    </h4>
-                  </div>
+                <div className="ml-4">
+                  <h4 className="text-md leading">{item}</h4>
                 </div>
               </div>
-            </div>
-            {/* </div> */}
-          </motion.div>
-        </ScrollAnimationWrapper>
-        {/* second column */}
-        <ScrollAnimationWrapper>
-          <motion.div
-            variants={scrollAnimation}
-            className="flex flex-col w-3/4 items-center mx-auto mt-4"
-            whileHover={{
-              scale: 1.1,
-              transition: {
-                duration: 0.1,
-              },
-            }}
-          >
-            {/* <div className="flex flex-col  w-2/5 items-center"> */}
-            <div>
-              <h2 className="text-2xl  text-center font-bold-700 md:text-start lg:text-start xl:text-start sm:text-3xl mb-8">
-                Industries:
-              </h2>
-              <div className="my-4 space-y-12 border border-2 border-gray-500 rounded-lg py-6 ">
-                <div className="flex items-center">
-                  <div className="flex-shrink-0">
-                    <div className="flex items-center justify-center w-12 h-12 rounded-md">
-                      <FaCheckCircle size={20} color="green" />
-                    </div>
-                  </div>
-                  <div className="ml-4">
-                    <h4 className="text-md font-medium leadi ">
-                      Certified and Sworn Translations, legally validated at
-                      Sworn Translators’ Association of the City of Buenos
-                      Aires*
-                    </h4>
+            ))}
+          </div>
+        </motion.div>
+
+        {/* Industries Card */}
+        <motion.div className="flex flex-col items-center  mt-4 w-2/4">
+          <h2 className="text-2xl text-center font-bold-700 md:text-start lg:text-start xl:text-start sm:text-3xl mb-8">
+            {t("indsubtitle")}
+          </h2>
+          <div className="my-4 space-y-10 border border-2 border-gray-500 rounded-lg py-6 w-3/4 h-full">
+            {t("industriesList", { returnObjects: true }).map((item, index) => (
+              <div key={index} className="flex items-center">
+                <div className="flex-shrink-0">
+                  <div className="flex items-center justify-center w-12 h-12 rounded-md">
+                    <FaCheckCircle size={20} color="#486f60" />
                   </div>
                 </div>
-                <div className="flex items-center">
-                  <div className="flex-shrink-0">
-                    <div className="flex items-center justify-center w-12 h-12 rounded-md">
-                      <FaCheckCircle size={20} color="green" />
-                    </div>
-                  </div>
-                  <div className="ml-4">
-                    <h4 className="text-md font-medium leadi ">
-                      Scientific and Technical Translations
-                    </h4>
-                  </div>
-                </div>
-                <div className="flex items-center">
-                  <div className="flex-shrink-0">
-                    <div className="flex items-center justify-center w-12 h-12 rounded-md">
-                      <FaCheckCircle size={20} color="green" />
-                    </div>
-                  </div>
-                  <div className="ml-4">
-                    <h4 className="text-md font-medium leadi ">Localization</h4>
-                  </div>
-                </div>
-                <div className="flex items-center">
-                  <div className="flex-shrink-0">
-                    <div className="flex items-center justify-center w-12 h-12 rounded-md">
-                      <FaCheckCircle size={20} color="green" />
-                    </div>
-                  </div>
-                  <div className="ml-4">
-                    <h4 className="text-md font-medium leadi ">
-                      Transcreation
-                    </h4>
-                  </div>
-                </div>
-                <div className="flex items-center">
-                  <div className="flex-shrink-0">
-                    <div className="flex items-center justify-center w-12 h-12 rounded-md">
-                      <FaCheckCircle size={20} color="green" />
-                    </div>
-                  </div>
-                  <div className="ml-4">
-                    <h4 className="text-md font-medium leadi ">
-                      Editing and Proofreading{" "}
-                    </h4>
-                  </div>
+                <div className="ml-4">
+                  <h4 className="text-md leading">{item}</h4>
                 </div>
               </div>
-            </div>
-            {/* </div> */}
-          </motion.div>
-        </ScrollAnimationWrapper>
+            ))}
+          </div>
+        </motion.div>
       </div>
-      <p className="text-center">
-        * The signature of our Sworn Translators is duly registered at Sworn
-        Translators’ Association of the City of Buenos Aires, which offers our
-        clients the ability to request a digitally certified project from
-        Translingual Solutions from the comfort of their home or office, with no
-        need to commute or handle procedures in person.{" "}
-      </p>
+
+      <p className="text-center">{t("disclaimer")}</p>
     </div>
   );
 };
