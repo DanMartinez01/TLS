deae74dd898ccbddee07ef9e6096155eef8ec9d7
Daniela <martinezdl_@hotmail.com>
2024-04-14 21:25:03 -0300

    refactor

--- components/Feature.js
+++ components/Feature.js
@@ -1,63 +1,13 @@
-// import Image from "next/image";
-// import React, { useMemo } from "react";
-// import { motion } from "framer-motion";
-// import getScrollAnimation from "../utils/getScrollAnimation";
-// import ScrollAnimationWrapper from "./Layout/ScrollAnimationWrapper";
-// import useCustomTranslation from "../utils/useCustomTranslation";
-
-// const Feature = () => {
-//   const scrollAnimation = useMemo(() => getScrollAnimation(), []);
-//   const { t } = useCustomTranslation();
-
-//   return (
-//     <div
-//       className="w-full h-2/4  sm:mb-14 py-16 px-6 sm:px-8 lg:px-16  bg-green"
-//       id="feature"
-//     >
-//       <div className="flex flex-col xl:flex-row justify-center items-center">
-//         <ScrollAnimationWrapper>
-//           <motion.div
-//             className="flex flex-col w-full xl:w-3/4"
-//             variants={scrollAnimation}
-//           >
-//             <h3 className="text-3xl lg:text-5xl font-medium leading-relaxed text-white-500 lg:mb-4">
-//               Translingual Solutions
-//             </h3>
-//             <p className="my-8 text-white-500 lg:leading-loose">
-//               {t("aboutUsText")}
-//             </p>
-//           </motion.div>
-//         </ScrollAnimationWrapper>
-//         <ScrollAnimationWrapper className="hidden xl:flex xl:w-full">
-//           <motion.div className="h-96 w-96" variants={scrollAnimation}>
-//             <Image
-//               src="/assets/logo-2.png"
-//               alt="Translingual Solutions"
-//               layout="responsive"
-//               quality={100}
-//               height={150}
-//               width={150}
-//               className="rounded-full"
-//             />
-//           </motion.div>
-//         </ScrollAnimationWrapper>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Feature;
-
 import Image from "next/image";
-import React, { useMemo } from "react";
+import React, { useMemo, useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import getScrollAnimation from "../utils/getScrollAnimation";
 import ScrollAnimationWrapper from "./Layout/ScrollAnimationWrapper";
-import useCustomTranslation from "../utils/useCustomTranslation";
+import { useTranslation } from "react-i18next";
 
 const Feature = () => {
   const scrollAnimation = useMemo(() => getScrollAnimation(), []);
-  const { t } = useCustomTranslation();
+  const { t } = useTranslation();
 
   const animationVariants = {
     hidden: {
@@ -93,14 +43,9 @@ const Feature = () => {
             >
               Translingual Solutions
             </motion.h3>
-            <motion.p
-              className="my-8 text-white-500 lg:leading-loose"
-              variants={animationVariants}
-              initial="hidden"
-              animate="visible"
-            >
-              {t("aboutUsText")}
-            </motion.p>
+            {t("aboutUs", { returnObjects: true }).map((item, index) => (
+              <p className="text-white-500 leading-relaxed">{item}</p>
+            ))}
           </motion.div>
         </ScrollAnimationWrapper>
         <ScrollAnimationWrapper className="hidden xl:flex xl:w-full">
