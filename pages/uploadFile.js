42e4f7d5875843f54187ea1bdd5c56e79a1b21ef
Daniela <martinezdl_@hotmail.com>
2024-04-14 23:34:56 -0300

    add language btn

--- pages/uploadFile.js
+++ pages/uploadFile.js
@@ -4,17 +4,19 @@ import Footer from "../components/Layout/Footer";
 import Upload from "../components/Upload";
 import { motion } from "framer-motion";
 import { FaArrowLeft } from "react-icons/fa";
+import LanguageSelector from "../components/LanguageSelector";
 
 const uploadFile = () => {
   return (
     <>
-      <div className="w-full h-24 flex justify-end items-center px-4 ">
+      <div className="w-full h-24 flex justify-end items-center px-4">
         <a href="/" className="hover:text-green">
           <span className="w-full flex flex-row items-center text-xl my-auto ">
             <FaArrowLeft />
             <p className="mx-4 hover:text-green"> Back to Home</p>
           </span>
         </a>
+        <LanguageSelector />
       </div>
       <div className="relative w-full h-72 overflow-hidden">
         <div
