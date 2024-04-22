8e02eda464b42b886260f29d0fc444f965cc2f7e
Daniela <martinezdl_@hotmail.com>
2024-04-22 00:19:43 -0300

    changes in email format

--- pages/api/send-email.js
+++ pages/api/send-email.js
@@ -4,18 +4,20 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async (req, res) => {
   const { pdfUrl, name, email, phone, moreDetails, language } = req.body;
+  const mail = process.env.EMAIL;
 
   try {
     const { data, error } = await resend.emails.send({
       from: "info@translingualsolutions.com",
-      to: "danimartinez233@gmail.com",
-      subject: "Cotizacion - PDF Attachment",
+      to: { mail },
+      subject: "TLS-New Request",
       html: `<div style="font-family: Arial, sans-serif; color: #333;">
       <p><strong>From:</strong> ${name}</p>
       <p><strong>PDF attachment link:</strong> ${pdfUrl}</p>
       <p><strong>Translate to:</strong> <span style="font-weight: bold;">${language}</span></p>
       <p><strong>Contact:</strong> ${phone}</p>
       <p><strong>More details:</strong> ${moreDetails}</p>
+      <img src="/favicon.png" alt="TLS" style="width: 100px; height: auto; margin-top: 20px;">
     </div>`,
       replyTo: email,
     });
