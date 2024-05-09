269313ce6b0d6a2e1aa7e07a2e80a3c3099db8b8
Daniela <martinezdl_@hotmail.com>
2024-05-08 21:13:58 -0300

    change img size

--- components/Hero.js
+++ components/Hero.js
@@ -13,24 +13,22 @@ const Hero = () => {
     <div className="max-w-screen-xl  px-8 xl:px-16 mx-auto" id="about">
       <ScrollAnimationWrapper>
         <motion.div
-          className="flex flex-col md:flex-row lg:flex-row xl:flex-row py-2 sm:py-16"
+          className="flex flex-col items-center md:flex-row lg:flex-row xl:flex-row py-2 sm:py-16"
           variants={scrollAnimation}
         >
-          <div className="flex flex-col justify-center items-center row-start-2 sm:row-start-1 mx-auto">
+          <div className="flex flex-col items-center row-start-2 sm:row-start-1 mx-auto">
             <h1 className="text-3xl lg:text-4xl xl:text-5xl font-medium text-black-600 leading-relaxed text-center">
               {t("formTitle")}
             </h1>
           </div>
-          <div className="flex w-2/4 mx-auto md:mx-auto lg:ml-auto xl:ml-auto">
+          <div className="flex w-2/4">
             <motion.div className="h-full  w-full" variants={scrollAnimation}>
               <Image
                 src="/assets/pc.jpg"
                 alt="Get a quote"
                 quality={100}
-                width={80}
-                height={80}
-                layout="responsive"
-                className="w-48 h-48"
+                width={360}
+                height={220}
               />
             </motion.div>
           </div>
