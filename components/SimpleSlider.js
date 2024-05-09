d22947c88bcc3a6b49c4b62b660a05071f9612ef
Daniela <martinezdl_@hotmail.com>
2024-05-08 21:15:23 -0300

    change button placement and img size

--- components/SimpleSlider.js
+++ components/SimpleSlider.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { useMemo } from "react";
 import { useRouter } from "next/router";
 import Slider from "react-slick";
 import Image from "next/image";
 import { motion } from "framer-motion";
+import getScrollAnimation from "../utils/getScrollAnimation";
+import ScrollAnimationWrapper from "./Layout/ScrollAnimationWrapper";
+import { useTranslation } from "react-i18next";
+import ButtonPrimary from "./misc/ButtonPrimary";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -18,34 +23,51 @@ const SimpleSlider = () => {
     autoplay: true, // Enable autoplay
     autoplaySpeed: 2500, // Set autoplay speed (in milliseconds)
   };
+  const scrollAnimation = useMemo(() => getScrollAnimation(), []);
+  const animationVariants = {
+    hidden: {
+      opacity: 0,
+      y: 40, // Move down by 50 pixels
+    },
+    visible: {
+      opacity: 1,
+      y: 0, // Move back to original position
+      transition: {
+        delay: 0.1, // Start animation after 0.2 seconds
+        duration: 0.1, // Animation duration is 0.8 seconds
+      },
+    },
+  };
 
   const router = useRouter();
+  const { t } = useTranslation();
 
   const handleButtonClick = () => {
     router.push("/uploadFile");
   };
 
   return (
-    <div className="w-full overflow-hidden mt-20  md:mt-10 relative">
+    <div className="w-full overflow-hidden mt-20  md:mt-10">
       <Slider {...settings}>
         <div className="w-full">
-          <Image src={"/assets/carousel1.png"} width={2000} height={1080} />
+          <Image src={"/assets/carousel1.png"} width={1900} height={720} />
         </div>
         <div className="w-full">
-          <Image src={"/assets/carousel2.png"} width={2000} height={1080} />
+          <Image src={"/assets/carousel2.png"} width={1900} height={720} />
         </div>
       </Slider>
-      <motion.button
-        className="absolute  left-1/2  top-3/4  w-2/5  
-        sm:w-40 sm:h-12  md:w-72 h-12 
-        md:h-16 max-w-3/4 mx-auto p-3 bg-green text-white-500 text-lg md:text-2xl rounded-lg hover:bg-white hover:text-green "
-        initial={{ x: "-100%", y: "-50%" }}
-        animate={{ x: "-50%", y: "-50%" }}
-        transition={{ duration: 0.8 }}
-        onClick={handleButtonClick}
-      >
-        Contact us
-      </motion.button>
+      <ScrollAnimationWrapper>
+        <motion.div
+          className="h-98 w-96 mx-auto  flex justify-center"
+          variants={scrollAnimation}
+        >
+          <ButtonPrimary
+            addClass="bg-green text-white  text-sm py-1"
+            children={t("contact")}
+            link="/uploadFile"
+          />
+        </motion.div>
+      </ScrollAnimationWrapper>
     </div>
   );
 };
